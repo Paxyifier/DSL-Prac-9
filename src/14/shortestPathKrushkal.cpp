db1a4d8ababa0adde282a3dcfd2fb37c0074e4de
Paxyifier <59118083+Paxyifier@users.noreply.github.com>
2023-02-08 16:04:14 +0000

    incomplete code, over to scrap probabaly

--- src/14/shortestPathKrushkal.cpp
+++ src/14/shortestPathKrushkal.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 template <typename T>
 class Node{
+    public:
     T data;
     Node<T> *next;
     Node(T data){
@@ -14,26 +15,31 @@ class Node{
 };
 template <typename T>
 class WeightedEdge{
+    public:
     Node<T>* node1;
     Node<T>* node2;
+    WeightedEdge<T>* next;
     int weight;
-    WeightedEdge<T>(Node<T> node1, Node<T> node2, int weight){
+    WeightedEdge<T>(Node<T> node1, Node<T> node2, int weight,WeightedEdge<T>* next =NULL){
         this->node1 = node1;
         this->node2 = node2;
         this->weight = weight;
+        this->next = next;
     }
 };
 
 template <typename T>
 class WeightedGraph{
-    Node<T> *head;
+    Node<T> *nodeHead;
+    WeightedEdge<T> *edgeHead;
     WeightedGraph<T>(){
-        this->head = NULL;
+        this->nodeHead = NULL;
+        this->edgeHead = NULL;
     }
     void addNode(T data){
         Node<T> *temp = this->head;
-        if (this->head ==NULL ){
-            this->head = new Node<T>(data);
+        if (this->nodeHead ==NULL ){
+            this->nodeHead = new Node<T>(data);
             return;
         }
         while (temp->next!=NULL){
@@ -41,7 +47,29 @@ class WeightedGraph{
         }
         temp->next = new Node<T>(data);
     }
+    Node<T>* findNode(T data){
+        if (this->nodeHead==NULL){
+            return NULL;
+        }
+        Node<T>* temp = this->nodeHead;
+        while (temp !=NULL){
+            if (temp->data == data){
+                return temp;
+            }
+            temp = temp->next;
+        }
+    }
     void addEdge(T data1, T data2, int weight){
-        
+        Node<T>* node1 = findNode(data1);
+        Node<T>* node2 = findNode(data2);
+        WeightedEdge<T> edge = new WeightedEdge<T>(node1, node2, weight),
+        temp=this->edgeHead;
+        if (this->edgeHead == NULL){
+            this->edgeHead = edge;
+            return;
+        }
+        while (this->temp->next !=NULL){
+            
+        }
     }
 };
